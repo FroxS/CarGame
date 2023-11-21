a2c6735cbffff3b9efd05285e76831adbb2406a8
FroxS <szulc.j.998@wp.pl>
2023-11-21 23:25:30 +0100

    Added car controls

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,8 @@ public class CarController : MonoBehaviour
 {
     public InputMenager im;
     public List<WheelCollider> throttleWeels;
-    public List<WheelCollider> steeringWeels;
+    public List<GameObject> steeringWeels;
+    public List<GameObject> meshes;
     public float strengthCoofficient = 2000f;
     public float maxTurn = 20f;
     public Transform CM;
@@ -24,6 +26,7 @@ public class CarController : MonoBehaviour
         {
             //rb.centerOfMass = CM.position;
         }
+        
     }
 
     // Update is called once per frame
@@ -34,9 +37,38 @@ public class CarController : MonoBehaviour
             wheel.motorTorque = strengthCoofficient * Time.deltaTime * im.throttles;
         }
 
-        foreach (WheelCollider wheel in steeringWeels)
+        foreach (GameObject wheel in steeringWeels)
         {
-            wheel.steerAngle = maxTurn * im.steer;
+            Quaternion initialParentRotation = wheel.transform.parent.rotation;
+            wheel.GetComponent<WheelCollider>().steerAngle = maxTurn * im.steer;
+
+            // Zapisz pierwotn¹ rotacjê obiektu nadrzêdnego na starcie
+            
+
+
+            // Oblicz now¹ rotacjê tylko dla osi Y, pozostawiaj¹c pozosta³e bez zmian
+            Quaternion newRotation = Quaternion.Euler(0, im.steer * maxTurn, 0);
+
+            // Kombinuj now¹ rotacjê z pierwotn¹ rotacj¹ obiektu nadrzêdnego
+            wheel.transform.rotation = newRotation * initialParentRotation;
+            //wheel.transform.localEulerAngles = new Vector3(wheel.transform.localEulerAngles.x,im.steer * maxTurn, wheel.transform.localEulerAngles.z);
+        }
+
+        foreach (GameObject mesh in meshes)
+        {
+            float rotationValue = rb.velocity.magnitude / (2 * Mathf.PI * 0.33f);
+
+            if(mesh?.tag?.Length == 3)
+            {
+                if (mesh.tag[2] == 'R')
+                    mesh.transform.Rotate(rb.velocity.magnitude * ((transform.InverseTransformDirection(rb.velocity).z >= 0 ? 1f : -1f) * -1f) / (2 * Mathf.PI * 0.33f), 0f, 0f);
+                else
+                    mesh.transform.Rotate(rb.velocity.magnitude * ((transform.InverseTransformDirection(rb.velocity).z >= 0 ? 1f : -1f) * 1f) / (2 * Mathf.PI * 0.33f), 0f, 0f);
+            }
+
+            
         }
     }
+
+    
 }
--- Assets/Scripts/InputMenager.cs
+++ Assets/Scripts/InputMenager.cs
@@ -7,22 +7,11 @@ public class InputMenager : MonoBehaviour
     public float throttles;
     public float steer;
 
-
-
     // Update is called once per frame
     void Update()
     {
-        // Pobierz input od gracza
-        //float horizontalInput = Input.GetAxis("Horizontal");
         throttles = Input.GetAxis("Vertical");
         steer = Input.GetAxis("Horizontal");
-
-        // Poruszanie do przodu i do ty³u
-        //transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime);
-
-        // Obracanie w lewo i w prawo
-        //transform.Rotate(Vector3.up * horizontalInput * rotationSpeed * Time.deltaTime);
     }
-
     
 }
